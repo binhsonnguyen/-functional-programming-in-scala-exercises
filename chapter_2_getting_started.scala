84ada3fea136d182dc93606ed112961198f2c1d7
sonnb1812 <sonnb@vnext.vn>
2017-05-25 15:50:01 +0700

    restructure answers

--- chapter_2_getting_started.scala
+++ chapter_2_getting_started.scala
@@ -1,9 +1,41 @@
 /*
-Implement isSorted, which checks whether an Array[A] is sorted according 
+EXERCISE 2.1
+Write a recursive function to get the nth Fibonacci number
+(http://mng.bz/C29s). The first two Fibonacci numbers are 0 
+and 1. The nth number is always the sum of the previous two—the 
+sequence begins 0, 1, 1, 2, 3, 5. Your definition should use a 
+local tail-recursive function.
+*/
+
+object Fibonacci {
+    def fib(n: Int): Int = {
+        @annotation.tailrec
+        def go(n: Int, p1: Int, p2: Int): Int = {
+            if (n <= 0) p1
+            else go(n - 1, p1 + p2, p1)
+        }
+        go(n, 1, 0)
+    }
+
+    def main(args: Array[String]): Unit = {
+        println(fib(0) == 1)
+        println(fib(1) == 1)
+        println(fib(2) == 2)
+        println(fib(3) == 3)
+        println(fib(4) == 5)
+        println(fib(5) == 8)
+        println(fib(6) == 13)
+    }
+}
+
+
+/*
+EXERCISE 2.2
+Implement `isSorted`, which checks whether an `Array[A]` is sorted according 
 to a given comparison function
 */
 
-object MyExercise {
+object ArrayAnalysis {
     def isSorted[A](as: Array[A], ordered: (A,A) => Boolean): Boolean = {
         @annotation.tailrec
         def iterator(n: Int): Boolean = {
@@ -37,3 +69,23 @@ object MyExercise {
         println(isSorted(Array(null, ""), stringComparator))
     }
 }
+
+
+/*
+EXERCISE 2.3
+Implement `curry` function
+*/
+def curry[A,B,C](f: (A, B) => C): A => B => C = a => b => f(a, b)
+
+/*
+EXERCISE 2.4
+Implement `uncurry` function
+*/
+def uncurry[A,B,C](f: A => B => C): (A, B) => C => (a: A, b: B) => f(a)(b)
+
+
+/*
+EXERCISE 2.5
+Implement the higher-order function that composes two functions.
+*/
+def compose[A,B,C](f: B => C, g: A => B): A => C = (a: A) => f(g(a))
