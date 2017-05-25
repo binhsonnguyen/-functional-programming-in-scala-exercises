168a86e77484fd56c1690ea5a5dfe6b77e944724
sonnb1812 <sonnb@vnext.vn>
2017-05-25 10:40:05 +0700

    exercise 2.1 instructions

--- 2.1/my-exercise.scala
+++ 2.1/my-exercise.scala
@@ -1,3 +1,11 @@
+/* 
+Write a recursive function to get the nth Fibonacci number
+(http://mng.bz/C29s). The first two Fibonacci numbers are 0 
+and 1. The nth number is always the sum of the previous two—the 
+sequence begins 0, 1, 1, 2, 3, 5. Your definition should use a 
+local tail-recursive function.
+*/
+
 object MyExercise {
     def fib(n: Int): Int = {
         def go(n: Int, p1: Int, p2: Int): Int = {
