8b6ddf8a3fbfdbc394bf4cab220fb2c467da144f
sonnb1812 <sonnb@vnext.vn>
2017-05-25 13:45:21 +0700

    refactor & add test cases

--- 2.2/my-exercise.scala
+++ 2.2/my-exercise.scala
@@ -15,11 +15,25 @@ object MyExercise {
     }
 
     def main(args: Array[String]): Unit = {
-        def intComparator(n1: Int, n2: Int): Boolean = {
-            n1 <= n2
-        }
+        val numbeComparator = (n1: Int, n2: Int) => n1 <= n2
+        println(isSorted(Array(), numbeComparator))
+        println(isSorted(Array(-1), numbeComparator))
+        println(isSorted(Array(-1, 2), numbeComparator))
+        println(isSorted(Array(-1, 2, 3), numbeComparator))
+        println(!isSorted(Array(2, 1), numbeComparator))
+        println(!isSorted(Array(1, 3, 2), numbeComparator))
 
-        val as = Array(-1, 2, 3)
-        println(isSorted(as, (n1: Int, n2: Int) => n1 <= n2))
+
+        val stringComparator = (s1: String, s2: String) => {
+            if (s1 == null && s2 == null) true
+            else if (s1 == null) true
+            else if (s2 == null) false
+            else s1.length <= s2.length
+        }
+        println(isSorted(Array(""), stringComparator))
+        println(isSorted(Array("", " "), stringComparator))
+        println(!isSorted(Array(" ", ""), stringComparator))
+        println(!isSorted(Array(" ", null), stringComparator))
+        println(isSorted(Array(null, ""), stringComparator))
     }
 }
