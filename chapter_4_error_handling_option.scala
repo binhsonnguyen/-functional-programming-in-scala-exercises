7bacf5dc1df05d33401c4ec8aa98cc24cbabb63c
sonnb1812 <sonnb@vnext.vn>
2017-07-11 21:52:49 +0700

    exercise 4.2

--- chapter_4_error_handling_option.scala
+++ chapter_4_error_handling_option.scala
@@ -1,8 +1,8 @@
 /**
  * EXERCISE 4.1
  */
-sealed trait MayBe[+A] {
-  def map[B](f: A => B): B = this match {
+sealed trait Maybe[+A] {
+  def map[B](f: A => B): Maybe[B] = this match {
     case None => None
     case Some(a) => Some(f(a))
   }
@@ -20,14 +20,14 @@ sealed trait MayBe[+A] {
   def flatMapViaMaybe[B](f: A => Maybe[B]): Maybe[B] = map(f).getOrElse(None)
 
   def orElse[B >: A](ob: => B): Maybe[B] = this match {
-    case None => Maybe(ob)
+    case None => Some(ob)
     case _ => this
   }
 
   def orElseViaViaMaybe[B >: A](ob: => B): Maybe[B] = map(a => Some(a)).getOrElse(None)
 
   def filter(f: A => Boolean): Maybe[A] = this match {
-    case Some(a) if (f(a)) => this
+    case Some(a) if f(a) => this
     case _ => None
   }
 
@@ -35,5 +35,18 @@ sealed trait MayBe[+A] {
 
 }
 
-case class Some[+A](get: A) extends MayBe[A]
-case object None extends MayBe[Nothing]
+case class Some[+A](get: A) extends Maybe[A]
+case object None extends Maybe[Nothing]
+
+/**
+ * Mặc dù luôn có thể sử dụng phiên bản pattern matching bình thường, nhưng chúng ta được kỳ vọng nên
+ * luôn sử dụng HOFs như ở các phiên bản hàm "Via...". Ta sẽ luyện tập việc này.
+ *
+ * EXERCISE 4.2
+ *
+ * Lợi dụng `flatMap`, triển khai hàm `variance` tính phương sai. Nếu trung bình của một sequence là m,
+ * phương sai là trung bình của `math.pow(x - m, 2)` trên mỗi `x` của sequence.
+ */
+def mean(xs: Seq[Double]): Maybe[Double] = if (xs.isEmpty) None else Some(xs.sum / xs.length)
+
+def variance(xs: Seq[Double]): Maybe[Double] = mean(xs).flatMap(m => mean(xs.map(x => math.pow(x - m, 2))))
