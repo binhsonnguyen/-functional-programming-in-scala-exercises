ca1dd5a6ead959c744daa81b9b6ad03a653d4ab3
sonnb1812 <sonnb@vnext.vn>
2017-07-17 10:18:33 +0700

    fix some syntax errors

--- chapter_3_functional_data_structure_tree.scala
+++ chapter_3_functional_data_structure_tree.scala
@@ -82,7 +82,7 @@ object Tree {
     *
     * Write a function depth that returns the maximum path length from the root of a tree to any leaf.
     */
-  def depth[A](t: Tree[A]) = t match {
+  def depth[A](t: Tree[A]): Int = t match {
     case Leaf(_) => 1
     case Branch(l, r) => 1 + (depth(l) max depth(r))
   }
@@ -94,7 +94,7 @@ object Tree {
     * a tree with a given function.
     */
   def map[A, B](t: Tree[A])(f: A => B): Tree[B] = t match {
-    case Leaf(_) => Leaf(f(_))
+    case Leaf(a) => Leaf(f(a))
     case Branch(l, r) => Branch(map(l)(f), map(r)(f))
   }
 
@@ -136,15 +136,19 @@ object Tree {
     * Reimplement them in terms of this more general function. Can you draw an analogy between this fold function
     * and the left and right folds for List?
     */
-  def fold[A, B](t: Tree[A])(f: A => B)(g: (B, B) => B) = t match {
-    case Leaf(_) => f(_)
+  def fold[A, B](t: Tree[A])(f: A => B)(g: (B, B) => B): B  = t match {
+    case Leaf(a) => f(a)
     case Branch(l, r) => g(fold(l)(f)(g), fold(r)(f)(g))
   }
 
-  def sizeViaFold[A](t: Tree[A]) = fold(t)(_ => 1)((l, r) => 1 + l + r)
+  def sizeViaFold[A](t: Tree[A]): Int = fold(t)(_ => 1)((l, r) => 1 + l + r)
 
-  def maximumViaFold(t: Tree[Int]) = fold(t)(_)((l, r) => l max r)
+  def maximumViaFold(t: Tree[Int]): Int = fold(t)(a => a)(_ max _)
 
-  def depthViaFold[A](t: Tree[A]) = fold(t)(_ => 0)((l, r) => 1 + (l max r))
+  def depthViaFold[A](t: Tree[A]): Int = fold(t)(_ => 0)((l, r) => 1 + (l max r))
+
+}
+
+def main(args: Array[String]): Unit = {
 
 }
